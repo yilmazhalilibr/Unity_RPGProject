e9f9a45f7fe933414f92fd9d330216f30dd57ad7
Halil İbrahim <77546808+yilmazhalilibr@users.noreply.github.com>
2023-08-11 10:24:50 +0300

    Add player controller and mover sc

---
+++ Assets/GameFolders/Scripts/Concrete/Controllers/PlayerController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity_RPGProject.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+    }
+}
+
---
+++ Assets/GameFolders/Scripts/Concrete/Movements/Mover.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unity_RPGProject.Movements
+{
+    public class Mover
+    {
+
+    }
+}
+
