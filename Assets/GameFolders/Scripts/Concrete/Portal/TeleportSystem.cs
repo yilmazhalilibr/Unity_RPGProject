2a133dd656220bcd37b9b7c1ecb5ee5c23f85c86
Halil İbrahim <77546808+yilmazhalilibr@users.noreply.github.com>
2023-09-09 10:54:57 +0300

    Teleport system done

--- Assets/GameFolders/Scripts/Concrete/Portal/TeleportSystem.cs
+++ Assets/GameFolders/Scripts/Concrete/Portal/TeleportSystem.cs
@@ -2,19 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity_RPGProject.Concrete.Controllers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class TeleportSystem 
+public class TeleportSystem
 {
 
-    public void TeleportVillage(PortalType type) 
+    public void TeleportVillage(PortalType type)
     {
         switch (type)
         {
             case PortalType.FirstVillage:
                 Debug.Log("FirstVillage ");
+                SceneManager.LoadScene("Game");
                 break;
             case PortalType.SecondVillage:
                 Debug.Log("SecondVillage ");
+                SceneManager.LoadScene("Game2");
                 break;
             default:
                 Debug.Log("Portal Error! No portal type!");
