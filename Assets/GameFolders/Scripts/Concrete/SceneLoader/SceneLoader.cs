e4717e50e88ea7edf4548de09571375f0a4ac213
Halil İbrahim <77546808+yilmazhalilibr@users.noreply.github.com>
2023-09-28 18:23:27 +0300

    Revert "Capturing multiple paramters to save system done"
    This reverts commit bc0833596495bdfd94b958a54fc9abb4b525ec81.

--- Assets/GameFolders/Scripts/Concrete/SceneLoader/SceneLoader.cs
+++ Assets/GameFolders/Scripts/Concrete/SceneLoader/SceneLoader.cs
@@ -17,10 +17,6 @@ namespace Unity_RPGProject.Concrete
         {
             SetSingletonThisGameObject(this);
         }
-        private void Start()
-        {
-            _ = SceneLoading(1);
-        }
         public async UniTask SceneLoading(int scene)
         {
 
